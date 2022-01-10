ccddd5be1ede37745b2d47fb9a2ab4d7cd237929
Kody2248 <kody2248@gmail.com>
2022-01-09 20:37:02 -0500

    Corrected image handling and radial bar

--- src/renderer/components/RadialBar.jsx
+++ src/renderer/components/RadialBar.jsx
@@ -12,16 +12,20 @@ import '../assets/css/RadialBar.scss';
  * @returns
  */
 const RadialBar = (props) => {
-  const { percent, label, size } = props;
-  const [percentState, setpercent] = useState(percent);
-  const [labelState, setlabel] = useState(label);
+  const { id, percent, label, size } = props;
 
   /**
    *
    * @param {object} options Defines SVG element, circumference of circle, stroke width and percentage to animate.
    */
   const createCircle = (options) => {
-    const { element, circleSize, stroke, percentage, label } = options;
+    const {
+      element,
+      circleSize,
+      stroke,
+      percentage,
+      label: labelOpt,
+    } = options;
     const width = element.attr('width');
     const height = element.attr('height');
 
@@ -61,7 +65,7 @@ const RadialBar = (props) => {
       .attr('x', width / 2)
       .attr('y', height / 2 + 24)
       .attr('text-anchor', 'middle')
-      .text(label);
+      .text(labelOpt);
   };
 
   const update = (options, delay = 0) => {
@@ -78,30 +82,33 @@ const RadialBar = (props) => {
   };
 
   useEffect(() => {
+    console.log('radial render');
     const svg = d3.select('.radial-graph');
     const options = {
       element: svg,
       circleSize: size * 0.33,
       stroke: 10,
-      percentage: percentState,
-      label: labelState,
+      percentage: percent,
+      label,
     };
     createCircle(options);
-    update(options);
+    update(options, 250);
     return () => {
       svg.selectAll('*').remove();
     };
-  });
+  }, [id]);
 
   return <svg className="radial-graph" height={size} width={size * 0.75} />;
 };
 
 RadialBar.propTypes = {
+  id: PropTypes.number,
   percent: PropTypes.number,
   label: PropTypes.string,
   size: PropTypes.string,
 };
 RadialBar.defaultProps = {
+  id: 1,
   percent: 0,
   label: 'Uptime',
   size: '300',
