0b68731c5cd4b09f020b5ac5f20926f9d1927575
Kody2248 <kody2248@gmail.com>
2022-01-12 00:35:41 -0500

    Added SQL Lite

--- src/main/database.ts
+++ src/main/database.ts
@@ -1,2 +1,57 @@
 import sqlite3 from 'sqlite3';
+import {Database} from 'sqlite3'
 import { open } from 'sqlite';
+import { promises } from 'stream';
+
+export default class Data {
+  db:Database;
+
+  connect(file: string) {
+    return new Promise<void>((resolve, reject) => {
+      this.db = new sqlite3.Database(file, (err) => {
+        if (err) {
+          reject(err.message);
+        }
+        console.log('Connected to database');
+        resolve();
+      });
+    });
+  }
+
+  createTables() {
+    return new Promise((resolve, reject)=>{
+      this.db.exec(`CREATE TABLE IF NOT EXISTS devices (
+                    device_id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    ip TEXT NOT NULL,
+                    port TEXT NULL,
+                    iconName TEXT NULL,
+                    iconPath TEXT NULL,
+                    iconSize TEXT NULL,
+                    iconType TEXT NULL,
+                  )`);
+
+      this.db.exec(`CREATE TABLE IF NOT EXISTS events (
+                    event_id INTEGER PRIMARY KEY,
+                    device INTEGER NOT NULL,
+                    action TEXT NOT NULL,
+                    outcome TEXT NOT NULL,
+                    dtTm TEXT NULL,
+                  )`);
+    });
+  }
+}
+
+const db = new Data();
+db.connect('./data.db')
+  .then(() => {
+    console.log('connected!');
+    return true;
+  })
+  .then(()=>{
+    console.log('created tables');
+    return true;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
