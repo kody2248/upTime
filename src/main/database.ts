369bd35313e20ca984f2f29bd50b642b9730973c
Kody2248 <kody2248@gmail.com>
2022-01-12 07:22:09 -0500

    Added SQL Lite

--- src/main/database.ts
+++ src/main/database.ts
@@ -1,25 +1,21 @@
-import sqlite3 from 'sqlite3';
-import {Database} from 'sqlite3'
+import sqlite3, { Database } from 'sqlite3';
 import { open } from 'sqlite';
 import { promises } from 'stream';
 
 export default class Data {
-  db:Database;
+  db: Database;
 
-  connect(file: string) {
-    return new Promise<void>((resolve, reject) => {
-      this.db = new sqlite3.Database(file, (err) => {
-        if (err) {
-          reject(err.message);
-        }
-        console.log('Connected to database');
-        resolve();
-      });
+  constructor(file: string) {
+    this.db = new sqlite3.Database(file, (err) => {
+      return this.createTables(err);
     });
   }
 
-  createTables() {
-    return new Promise((resolve, reject)=>{
+  createTables(err: Error | null) {
+    return new Promise<void>((resolve, reject) => {
+      if (err) {
+        reject(err);
+      }
       this.db.exec(`CREATE TABLE IF NOT EXISTS devices (
                     device_id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL,
@@ -28,7 +24,7 @@ export default class Data {
                     iconName TEXT NULL,
                     iconPath TEXT NULL,
                     iconSize TEXT NULL,
-                    iconType TEXT NULL,
+                    iconType TEXT NULL
                   )`);
 
       this.db.exec(`CREATE TABLE IF NOT EXISTS events (
@@ -36,22 +32,33 @@ export default class Data {
                     device INTEGER NOT NULL,
                     action TEXT NOT NULL,
                     outcome TEXT NOT NULL,
-                    dtTm TEXT NULL,
+                    dtTm TEXT NULL
                   )`);
+      resolve();
     });
   }
 }
 
-const db = new Data();
-db.connect('./data.db')
+// const db = new Data();
+// db.connect('./data.db')
+//   .then(() => {
+//     console.log('connected!');
+//     return true;
+//   })
+//   .then(() => {
+//     console.log('created tables');
+//     return true;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const db = new Data('./data.db')
   .then(() => {
-    console.log('connected!');
-    return true;
-  })
-  .then(()=>{
     console.log('created tables');
     return true;
   })
-  .catch((err) => {
-    console.log(err);
+  .catch(() => {
+    console.log('could not connect to tables');
   });
+console.log(db);
